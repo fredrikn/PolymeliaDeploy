9000d384de1345f42643e4dee1768bf637863396
fredrikn <fnormen@hotmail.com>
2013-07-01 16:12:23 +0200

    Made the AgentEnvironment conent ThreadStatic.

--- src/PolymeliaDeploy/AgentEnvironment.cs
+++ src/PolymeliaDeploy/AgentEnvironment.cs
@@ -3,12 +3,31 @@
     using System.Collections.Generic;
 
     using PolymeliaDeploy.Data;
+    using System;
+
 
     public static class AgentEnvironment
     {
+        [ThreadStatic]
         private static string _serverRole = string.Empty;
+        [ThreadStatic]
+        private static IDictionary<string, string> _variables;
 
-        public static IDictionary<string, string> Variables { get; set; }
+        [ThreadStatic]
+        private static long? _currentActivityId;
+
+        [ThreadStatic]
+        private static long _taskId;
+
+        [ThreadStatic]
+        private static string _deployVersion;
+
+
+        public static IDictionary<string, string> Variables
+        { 
+            get { return _variables; }
+            set { _variables = value; }
+        }
 
 
         public static string ServerRole
@@ -17,10 +36,25 @@
             set { _serverRole = value; }
         }
 
-        public static long? CurrentActivityId { get; set; }
 
-        public static long TaskId { get; set; }
+        public static long? CurrentActivityId
+        {
+            get { return _currentActivityId; }
+            set { _currentActivityId = value; }
+        }
 
-        public static string DeployVersion { get; set; }
+
+        public static long TaskId
+        {
+            get { return _taskId; }
+            set { _taskId = value; }
+        }
+
+
+        public static string DeployVersion
+        {
+            get { return _deployVersion;  }
+            set { _deployVersion = value; }
+        }
     }
 }
